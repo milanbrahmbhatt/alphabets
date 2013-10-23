424436d8f6d4a4f5a2084cd9c0f7a4d03a987224
Milan Brahmbhatt <mbrahmbhatt@zynga.com>
2013-10-23 11:46:17 -0700

    initial commit

---
+++ alphabets/alphabets/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  alphabets
+//
+//  Created by Milan Brahmbhatt on 10/23/13.
+//  Copyright (c) 2013 Milan Brahmbhatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
