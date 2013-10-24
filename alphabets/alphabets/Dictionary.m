8e0b9d894204da208ee2fd6123af44f894805658
Mark Kim <mkim@zynga.com>
2013-10-23 21:02:02 -0700

    adding Word and Dictionary models

---
+++ alphabets/alphabets/Dictionary.m
@@ -0,0 +1,14 @@
+//
+//  Dictionary.m
+//  alphabets
+//
+//  Created by Mark Kim on 10/23/13.
+//  Copyright (c) 2013 Milan Brahmbhatt. All rights reserved.
+//
+
+#import "Dictionary.h"
+
+@implementation Dictionary
+
+
+@end
---
+++ alphabets/alphabets/Word.m
@@ -0,0 +1,13 @@
+//
+//  Word.m
+//  alphabets
+//
+//  Created by Mark Kim on 10/23/13.
+//  Copyright (c) 2013 Milan Brahmbhatt. All rights reserved.
+//
+
+#import "Word.h"
+
+@implementation Word
+
+@end
